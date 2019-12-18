1f7d0fdef29abeb4fb99243ee633847eb2da0095
chandryou <applegna@gmail.com>
2019-12-18 13:37:02 +0900

    add suga processiong

--- R/preprocesssing.r
+++ R/preprocesssing.r
@@ -26,15 +26,17 @@
 #' @param deviceName      A column name for device.
 #' @param dateName        A column name for start date.
 #' @param materialName        A column name for the material of device.
+#' @param KoreanDictFile  Path for csv file containing translation between Korean and English. If you don't want to translate, please set this value as NULL
 #'
 #' @export
 DeviceProcess<-function(exelFilePath,
-                        sheetName = "",
+                        sheetName,
                         materialData=NULL,
-                        deviceCode = "",
-                        deviceName = "",
-                        startDateName="",
-                        materialName = ""
+                        deviceCode,
+                        deviceName,
+                        startDateName,
+                        materialName,
+                        KoreanDictFile="./inst/csv/tmt_Eng_Kor_translation.csv"
 ){
   if(is.null(materialData)){
     matData <- readxl::read_excel(exelFilePath,
@@ -72,12 +74,142 @@ DeviceProcess<-function(exelFilePath,
                     material=material,
                     dosage=NA,
                     dosageUnit=NA,
-                    sanjungName = NA
+                    sanjungName = NA,
+                    stringsAsFactors=FALSE
   )
 
   matDf$validStartDate<-lubridate::as_date(matDf$validStartDate)
   matDf$validEndDate<-lubridate::as_date(matDf$validEndDate)
 
-  class(matDf)<-"ediDf"
   return(matDf)
+}
+
+#' Pre-processing for SUGA
+#'
+#' @details
+#' Pre-processing for the Excel EDI file
+#'
+#' @param exelFilePath    file path for the excel file.
+#' @param sheetName       A sheet name of interest.
+#' @param sugaData        Prepared material data. Default is NULL.
+#' @param sugaCode        A column name for device EDI code.
+#' @param KoreanName      A column name for device.
+#' @param EnglishName     A column name for device.
+#' @param startDateName   A column name for start date.
+#' @param sanjungName     A column name for the material of device.
+#' @param KoreanDictFile  Path for csv file containing translation between Korean and English. If you don't want to translate, please set this value as NULL
+#'
+#' @export
+SugaProcess<-function(exelFilePath,
+                      sheetName,
+                      sugaData=NULL,
+                      sugaCode,
+                      KoreanName,
+                      EnglishName,
+                      startDateName,
+                      sanjungName,
+                      KoreanDictFile="./inst/csv/suga_Eng_Kor_translation.csv"
+){
+  if(is.null(sugaData)){
+    sugaData <- readxl::read_excel(exelFilePath,
+                                   sheet=sheetName,
+                                   col_names = TRUE)
+  }
+
+  #colnames(mdcData)<-SqlRender::snakeCaseToCamelCase(colnames(mdcData))
+  conceptCode <- dplyr::pull(sugaData, sugaCode)
+  conceptSynonym <- dplyr::pull(sugaData, KoreanName)
+  conceptName <- dplyr::pull(sugaData, EnglishName)
+  startDate <- dplyr::pull(sugaData, startDateName)
+  sanjungName <- dplyr::pull(sugaData, sanjungName)
+
+  ancestorConceptCode <- dplyr::pull(sugaData, sugaCode)
+
+  #substring upto 5 letters to make ancestors
+  ancestorConceptCode<-substring(ancestorConceptCode,1,5)
+  conceptClassId <- c()
+
+  sugaDf <- data.frame(conceptCode= conceptCode,
+                       conceptName = conceptName,
+                       conceptSynonym=conceptSynonym,
+                       domainId = "Procedure",
+                       vocabularyId = "Korean EDI",
+                       conceptClassId = "Procedure",
+                       validStartDate = ifelse( is.na(startDate),"1970-01-01", as.character(startDate)),
+                       validEndDate = "2099-12-31",
+                       invalidReason = NA,
+                       ancestorConceptCode=ancestorConceptCode,
+                       previousConceptCode=NA,
+                       material=NA,
+                       dosage=NA,
+                       dosageUnit=NA,
+                       sanjungName = sanjungName,
+                       stringsAsFactors=FALSE)
+
+  #If ancestor is identical to concpet code, then remove the ancestor code
+  sugaDf$ancestorConceptCode[sugaDf$ancestorConceptCode==sugaDf$conceptCode] <- NA
+
+  #If ancestor is not included to concpet code, then remove the ancestor code
+  sugaDf$ancestorConceptCode[!sugaDf$ancestorConceptCode %in% sugaDf$conceptCode] <- NA
+
+  ##Set domain ID
+  sugaDf$domainId[grepl("^A[AH]",sugaDf$conceptCode) |
+                    grepl("^[BCEFG]",sugaDf$conceptCode) |
+                    grepl("^H[AC]",sugaDf$conceptCode) |
+                    grepl("^FA",sugaDf$conceptCode) ] <- "Measurement"
+
+  ##Set concept class ID
+  sugaDf$conceptClassId[nchar(sugaDf$conceptCode)>nchar(sugaDf$ancestorConceptCode)] <- "Procedure Details"
+
+  sugaDf$conceptClassId[(sugaDf$domainId=="Measurement") & (sugaDf$conceptClassId=="Procedure")] <- "Measurement"
+  sugaDf$conceptClassId[(sugaDf$domainId=="Measurement") & (sugaDf$conceptClassId=="Procedure Details")] <- "Measurement Details"
+
+  if(!is.null(KoreanDictFile)) {
+    #nrow(sugaDf2) #270413
+    #translation for concept name
+    dict<- read.csv(KoreanDictFile, stringsAsFactors= FALSE)
+    colnames(dict)[grepl("conceptName",colnames(dict))] <- "conceptNameTr"
+    if(!length(unique(dict$conceptSynonym)) == length(dict$conceptSynonym)) stop ("Korean names in the dictionary should be unique")
+    sugaDf <- merge(sugaDf,dict,by= "conceptSynonym", all.x = TRUE, all.y = FALSE)
+
+    sugaDf$conceptName <- ifelse(is.na(sugaDf$conceptName), sugaDf$conceptNameTr,sugaDf$conceptName)
+
+    #translation for sanjung name
+    dict<- read.csv(KoreanDictFile, stringsAsFactors= FALSE)
+    colnames(dict)[grepl("conceptSynonym",colnames(dict))] <- "sanjungName"
+    colnames(dict)[grepl("conceptName",colnames(dict))] <- "sanjungTr"
+
+    sugaDf <- merge(sugaDf,dict,by= "sanjungName", all.x = TRUE, all.y = FALSE)
+    #sugaDf2$sanjungTr[is.na(sugaDf2$sanjungTr)]<-""
+    sugaDf$conceptName <- ifelse(!is.na(sugaDf$sanjungTr),
+                                 paste(sugaDf$conceptName,sugaDf$sanjungTr,sep=","),
+                                 sugaDf$conceptName)
+  }
+  #remove unnecessary columns
+  sugaDf<-sugaDf[c("conceptCode", "conceptName", "conceptSynonym", "domainId", "vocabularyId", "conceptClassId",
+                   "validStartDate", "validEndDate", "invalidReason","ancestorConceptCode","previousConceptCode",
+                   "material", "dosage", "dosageUnit","sanjungName")]
+
+  return(sugaDf)
+}
+
+#' Pre-processing for DRUG
+#'
+#' @details
+#' Pre-processing for the Excel EDI file
+#'
+#' @param exelFilePath    file path for the excel file.
+#' @param sheetName       A sheet name of interest.
+#' @param sugaData        Prepared material data. Default is NULL.
+#' @param sugaCode        A column name for device EDI code.
+#' @param KoreanName      A column name for device.
+#' @param EnglishName     A column name for device.
+#' @param startDateName   A column name for start date.
+#' @param sanjungName     A column name for the material of device.
+#' @param KoreanDict      A csv file translating between Korean and English
+#'
+#' @export
+#'
+drugEdiProcess<-function(){
+
 }
